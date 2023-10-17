f9f92e65b2be0a21aec040e1b3221b7dc628e622
GrubPL <141992850+GrubPL@users.noreply.github.com>
2023-10-17 21:15:54 +0200

    Somewhat Updated modmanager.bat
    Added Some Shitty feature That may even not work for everyone
    
    and added some credit do me without removing the orginal Credits

--- ModManager.bat
+++ ModManager.bat
@@ -17,9 +17,12 @@ ECHO ...............................................
 ECHO Portal 2 VR mod manager
 ECHO Mod by Gistix https://github.com/Gistix/portal2vr
 ECHO Manager by Juli https://julimiro.carrd.co
+ECHO This Crap Fork By Grub (no link)
 ECHO.
 ECHO Disclosure : I don't know how to code batch.
 ECHO I am a guy on the internet with a Google and limited coding knowledge
+echo
+echo (The creator of this Fork also is a guy on the internet and limited coding knowlage)
 echo.
 echo.
 echo Note : Please do configuration before anything else.
@@ -29,15 +32,17 @@ ECHO.
 ECHO 1 - Enable VR
 ECHO 2 - Disable VR
 ECHO 3 - Launch game
-echo 4 - Configure
-ECHO 5 - Exit
+echo 4 - Launch Game For VR (Its Experimental Because It Will Break Co-op, You will Need to enable the mod first)
+ECHO 5 - Configure
+echo 6 - Exit
 ECHO.
-SET /P M=Type 1, 2, 3, 4, or 5 then press ENTER: 
+SET /P M=Type 1, 2, 3, 4, 5 or 6 then press ENTER: 
 IF %M%==1 GOTO enable
 IF %M%==2 GOTO disable
 IF %M%==3 GOTO launch
-IF %M%==5 GOTO EOF
-if %M%==4 goto configure
+IF %M%==4 GOTO launchexp
+IF %M%==5 GOTO configure
+if %M%==6 goto EOF
 cls
 echo Incorrect option.
 
@@ -47,9 +52,6 @@ goto menu
 cd ..
 echo Your dlc folder is where you installed the contents of portal2_dlc3
 echo Currently %dlcfolder%
-echo "Please include the whole folder name including portal2_dlc<number>"
-echo Example : portal2_dlc3 
-echo NOT just 3
 set /p Input=What is your dlc folder? : 
 del manager_config.txt
 echo  %Input% >> manager_config.txt
@@ -68,7 +70,6 @@ echo Renaming openvr_api.dll.bak to openvr_api.dll
 move openvr_api.dll.bak openvr_api.dll
 cd ..
 cd %dlcfolder%
-echo Renaming pak01_dir.vpk.bak to pak01_dir.vpk
 move pak01_dir.vpk.bak pak01_dir.vpk
 cd ..
 cd bin
@@ -85,7 +86,6 @@ move openvr_api.dll openvr_api.dll.bak
 
 cd ..
 cd %dlcfolder%
-echo Renaming pak01_dir.vpk to pak01_dir.vpk.bak
 move pak01_dir.vpk pak01_dir.vpk.bak
 cd ..
 cd bin
@@ -94,4 +94,9 @@ goto menu
 :launch
 cls
 start steam://rungameid/620
+goto menu
+
+:launchexp
+cls
+start /d .. portal2.exe -insecure -window -novid +mat_motion_blur_percent_of_screen_max 0 +mat_queue_mode 0 +mat_vsync 0 +mat_antialias 0 +mat_grain_scale_override 0 -width 1280 -height 720
 goto menu
